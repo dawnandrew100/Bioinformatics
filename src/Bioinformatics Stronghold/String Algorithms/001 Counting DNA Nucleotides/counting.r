37dd89449adc598397791afc05107ad393bfd412
Andrew Hennis <81731313+dawnandrew100@users.noreply.github.com>
2025-02-12 12:49:29 -0500

    R Solution to problem 1

---
+++ src/Bioinformatics Stronghold/String Algorithms/001 Counting DNA Nucleotides/counting.r
@@ -0,0 +1,16 @@
+library(stringr)
+
+str = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+nucleotides <- c("A", "C", "G", "T")
+nucleotide_count <- numeric()
+
+for(nuc in nucleotides){
+    nucleotide_count = c(nucleotide_count, str_count(str, nuc))
+}
+
+str.data = data.frame(
+    nuc = nucleotides,
+    nuc_count = nucleotide_count
+)
+
+print(str.data, row.names=FALSE)
