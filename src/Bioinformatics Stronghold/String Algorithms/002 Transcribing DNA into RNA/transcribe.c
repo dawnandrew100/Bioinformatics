267ecf38a742d209898767c73c0ed0e668cec01f
Andrew Hennis <81731313+dawnandrew100@users.noreply.github.com>
2025-02-13 15:05:53 -0500

    C solution to Problem 2

---
+++ src/Bioinformatics Stronghold/String Algorithms/002 Transcribing DNA into RNA/transcribe.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *to_transcribe(char *seq);
+
+int main(){
+    char *aa = "GATGGAACTTGACTACGTAAATT";
+    char *transcribed = to_transcribe(aa);
+    if(transcribed){
+        printf("%s\n", transcribed);
+        free(transcribed);
+    }
+    return 0;
+}
+
+char* to_transcribe(char *seq){
+    int length = strlen(seq);
+    char *t_seq = (char*)malloc(length*sizeof(char));
+    if(!t_seq){
+        return NULL;
+    }
+    for(int i=0; seq[i]!='\0'; i++){
+        if(seq[i] == 'T'){
+            t_seq[i] = 'U';
+        } else {
+            t_seq[i] = seq[i];
+        }
+    }
+    return t_seq;
+}
