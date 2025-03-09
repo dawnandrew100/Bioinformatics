f8cbc93d31fc3d4652cfa92fbf5dc66aeb835da2
Andrew Hennis <dawnandrew100@gmail.com>
2025-03-09 11:04:12 -0400

    Problem 19 Solution

---
+++ src/Bioinformatics Stronghold/Combinatorics/019 Enumerating Gene Orders/geneorder.py
@@ -0,0 +1,17 @@
+from itertools import permutations
+
+def main():
+    n = 7
+    members = range(1, n+1)
+    results = []
+    for permutation in permutations(members):
+        temp = [str(x) for x in permutation]
+        results.append(" ".join(temp))
+
+    with open("results.txt", "w") as file:
+        file.write(f"{str(len(results))}\n")
+        for result in results:
+            file.write(f"{result}\n")
+
+if __name__ == "__main__":
+    main()
