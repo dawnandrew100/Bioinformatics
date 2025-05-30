e260cf025184d8ec3fd4059b514254eb62b735fa
Andrew Hennis <dawnandrew100@gmail.com>
2025-05-30 18:24:14 -0400

    formatting

--- src/Project Euler/4/large-palindrome/src/main.rs
+++ src/Project Euler/4/large-palindrome/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
-    let upper_lim = 999*999;
-    let lower_lim = 100*100;
+    let upper_lim = 999 * 999;
+    let lower_lim = 100 * 100;
     let target_count = 3;
     println!("{}", largest_palindrome(upper_lim, lower_lim, target_count));
 }
 
 fn largest_palindrome(max_num: usize, min_num: usize, t_count: usize) -> usize {
     let mut option: usize = max_num;
-    
+
     while option > min_num {
         let opt = option.to_string();
         let is_palindrome = check_is_palindrome(opt);
@@ -16,14 +16,17 @@ fn largest_palindrome(max_num: usize, min_num: usize, t_count: usize) -> usize {
             continue;
         }
         let factors = gen_all_factors(option);
-        
-        let n_chars: Vec<usize> = factors.iter()
-                            .cloned()
-                            .filter(|&n| n.to_string().chars().count() == t_count)
-                            .collect();
+
+        let n_chars: Vec<usize> = factors
+            .iter()
+            .cloned()
+            .filter(|&n| n.to_string().chars().count() == t_count)
+            .collect();
         if factors.len() % 2 != 0 {
             let mid = factors.len() / 2;
-            if factors[mid] * factors[mid] == option && factors[mid].to_string().chars().count() == t_count {
+            if factors[mid] * factors[mid] == option
+                && factors[mid].to_string().chars().count() == t_count
+            {
                 println!("{}", factors[mid]);
                 return option;
             }
@@ -60,7 +63,7 @@ fn check_is_palindrome(word: String) -> bool {
 fn gen_all_factors(target: usize) -> Vec<usize> {
     let sqrt_n = (target as f64).sqrt() as usize;
     let mut factors = Vec::new();
-    
+
     for i in 1..=sqrt_n {
         if target % i == 0 {
             factors.push(i);
@@ -77,9 +80,9 @@ fn factors_eq_palindrome(factors: &Vec<usize>, target: usize) -> (bool, Vec<usiz
     let mut l: usize = 0;
     let mut r: usize = l + 1;
     let mut fac_vec: Vec<usize> = Vec::new();
-    
+
     if factors.len() == 1 {
-        if factors[0] * factors [0] == target {
+        if factors[0] * factors[0] == target {
             fac_vec.push(factors[0]);
             return (true, fac_vec);
         }
@@ -89,7 +92,7 @@ fn factors_eq_palindrome(factors: &Vec<usize>, target: usize) -> (bool, Vec<usiz
         if r >= factors.len() {
             l += 1;
             r = l + 1;
-            if r == factors.len(){
+            if r == factors.len() {
                 break;
             }
         }
